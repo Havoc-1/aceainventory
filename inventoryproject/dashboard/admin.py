0fe9c7c73988187af1c6a6e53c7be59155647c66
Meepster212 <91581727+Meepster212@users.noreply.github.com>
2022-04-16 00:17:07 +0800

    Admin Panel Revamp w/ Order and Product Models

--- inventoryproject/dashboard/admin.py
+++ inventoryproject/dashboard/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Equipment, Deliveries
+from django.contrib.auth.models import Group
+
+admin.site.site_header = 'ACEA Inventory Admin Dashboard'
+
+class EquipAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'status',)
+    list_filter = ['type']
 
 # Register your models here.
+admin.site.register(Equipment, EquipAdmin)
+admin.site.register(Deliveries)
--- inventoryproject/dashboard/models.py
+++ inventoryproject/dashboard/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+TYPE = (
+    ('DefBars', 'DefBars'),
+    ('Tools', 'Tools'),
+)
+
+class Equipment(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    location = models.CharField(max_length=100, null=True)
+    type = models.CharField(max_length=100, choices=TYPE, null=True)
+    brand = models.CharField(max_length=100, null=True)
+    status = models.CharField(max_length=100, null=True)
+    quantity = models.PositiveIntegerField(null=True)
+    remarks = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return f'{self.name}--{self.location}--{self.status}'
+
+class Deliveries(models.Model):
+    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE, null=True)
+    staff = models.ForeignKey(User, models.CASCADE, null=True)
+    delivery_quantity = models.PositiveIntegerField(null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Deliveries'
+
+    def __str__(self):
+        return f'{self.equipment} ordered by {self.staff.username}'
