861ca2e3505fcccc00dd0a57d1ad91958c348396
Meepster212 <91581727+Meepster212@users.noreply.github.com>
2022-04-19 15:04:33 +0800

    User Registration + comments + styling changes

--- inventoryproject/dashboard/admin.py
+++ inventoryproject/dashboard/admin.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import Group
 
 admin.site.site_header = 'ACEA Inventory Admin Dashboard'
 
-class EquipAdmin(admin.ModelAdmin):
-    list_display = ('name', 'location', 'status',)
-    list_filter = ['type']
+class EquipAdmin(admin.ModelAdmin):                     
+    list_display = ('name', 'location', 'status',)      # displays the equipment model with the following list
+    list_filter = ['type']                              # can filter by type ()
 
 # Register your models here.
 admin.site.register(Equipment, EquipAdmin)
