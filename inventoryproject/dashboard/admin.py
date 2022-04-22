414ef85bfabf1542d35895d69a06db5c9bf2ef87
Meepster212 <91581727+Meepster212@users.noreply.github.com>
2022-04-22 18:32:24 +0800

    Inventory Functionality

--- inventoryproject/dashboard/admin.py
+++ inventoryproject/dashboard/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from .models import Equipment, Deliveries
+from .models import Inventory, Deliveries
 from django.contrib.auth.models import Group
 
 admin.site.site_header = 'ACEA Inventory Admin Dashboard'
 
-class EquipAdmin(admin.ModelAdmin):                     
-    list_display = ('name', 'location', 'status',)      # displays the equipment model with the following list
+class InventoryAdmin(admin.ModelAdmin):                     
+    list_display = ('name', 'location', 'quantity', 'status',)      # displays the inventory model with the following list
     list_filter = ['type']                              # can filter by type ()
 
 # Register your models here.
-admin.site.register(Equipment, EquipAdmin)
+admin.site.register(Inventory, InventoryAdmin)
 admin.site.register(Deliveries)
--- inventoryproject/dashboard/urls.py
+++ inventoryproject/dashboard/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     path('dashboard/', views.index, name='dashboard-index'),
     path('staff/', views.staff, name='dashboard-staff'),
-    path('equipment/', views.equipment, name='dashboard-equipment'),
+    path('inventory/', views.inventory, name='dashboard-inventory'),
+    path('inventory/delete/<int:pk>/', views.inventory_delete, name='dashboard-inventory-delete'),              # <int:pk> shows the id of deleted item
+    path('inventory/edit/<int:pk>/', views.inventory_edit, name='dashboard-inventory-edit'),              # <int:pk> shows the id of deleted item
     path('deliveries/', views.deliveries, name='dashboard-deliveries'),
 ]
 
